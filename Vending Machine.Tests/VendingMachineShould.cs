11af455971abe8da02733a106c3c70b15f6e4fbe
Saravasha <33827803+Saravasha@users.noreply.github.com>
2022-07-15 15:37:37 +0200

    Add project files.

---
+++ Vending Machine.Tests/VendingMachineShould.cs
@@ -0,0 +1,11 @@
+namespace Vending_Machine.Tests
+{
+    public class VendingMachineShould
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Vending Machine/IVending.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vending_Machine
+{
+    internal interface IVending
+    {
+
+        public void Purchase()
+        {
+
+        }
+
+        public void ShowAll()
+        {
+
+        }
+
+        public void InsertMoney()
+        {
+
+        }
+        
+        public void EndTransaction()
+        {
+
+        }
+    }
+}
